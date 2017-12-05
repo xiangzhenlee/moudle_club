2e0e9cec9eebb0f9f21906428f830604977d498d
lixz <lixz@wanbu.com.cn>
2017-12-05 13:49:54 +0800

    配置模块话开发和集成模式下的清单文件

--- src/main/java/com/yushan/module_club/MainActivity.java
+++ src/main/java/com/yushan/module_club/MainActivity.java
@@ -3,6 +3,8 @@ package com.yushan.module_club;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.yushan.gitmoudledemo.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
