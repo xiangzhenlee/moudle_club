6463261046b8b540b9c5c0ccbd1cb254a902dbc2
lixz <lixz@wanbu.com.cn>
2017-12-08 10:40:25 +0800

    实验SourceTree上传子模块代码

--- src/main/java/com/yushan/module_club/activity/ClubActivity.java
+++ src/main/java/com/yushan/module_club/activity/ClubActivity.java
@@ -2,6 +2,7 @@ package com.yushan.module_club.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.yushan.module_club.R;
@@ -13,5 +14,6 @@ public class ClubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
+        Log.e("yushan","实验SourceTree");
     }
 }
