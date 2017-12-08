70cd8e32e375bddff3078e38f4d91b285210c725
lixz <lixz@wanbu.com.cn>
2017-12-08 09:17:07 +0800

    添加模块启动Application

--- src/main/java/com/yushan/module_club/activity/ClubActivity.java
+++ src/main/java/com/yushan/module_club/activity/ClubActivity.java
@@ -1,9 +1,10 @@
-package com.yushan.module_club;
+package com.yushan.module_club.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.yushan.module_club.R;
 
 @Route(path = "/module_club/ClubActivity")
 public class ClubActivity extends AppCompatActivity {
---
+++ src/main/java/com/yushan/module_club/debug/ClubApplication.java
@@ -0,0 +1,19 @@
+package com.yushan.module_club.debug;
+
+import android.os.Bundle;
+
+import com.yushan.lib_base.base.BaseActivity;
+import com.yushan.lib_base.base.BaseApplication;
+import com.yushan.module_club.R;
+
+/**
+ * Created by beiyong on 2017-12-8.
+ */
+
+public class ClubApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
